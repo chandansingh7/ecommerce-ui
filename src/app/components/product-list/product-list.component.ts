bb0bb1e625d80bb976b4e7bb97f2fe440950a25c
chandan <chandan.singh558@gmail.com>
2024-05-01 14:36:19 -0500

    Initial Release

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit} from '@angular/core';
+import {ProductService} from "../../services/product.service";
+import {Product} from "../../common/product";
+import {CurrencyPipe, NgForOf} from "@angular/common";
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [
+    CurrencyPipe,
+    NgForOf
+  ],
+  //templateUrl: './product-list.component.html',
+  templateUrl: './product-list-table.component.html',
+  styleUrl: './product-list.component.scss'
+})
+export class ProductListComponent implements OnInit{
+
+  product: Product[] = [];
+  constructor(private productService: ProductService) {
+  }
+
+  ngOnInit(){
+    this.listProducts()
+  }
+
+  listProducts(){
+    this.productService.getProductList().subscribe(data => {
+      this.product = data
+    })
+  }
+}
