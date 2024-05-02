7b2313c68031f5b28c0b0faa838a44117c1bbaf5
chandan <chandan.singh558@gmail.com>
2024-05-01 19:39:05 -0500

    Dynamic Search of product by category id

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ProductService} from "../../services/product.service";
 import {Product} from "../../common/product";
 import {CurrencyPipe, NgForOf} from "@angular/common";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-product-list',
@@ -10,22 +11,42 @@ import {CurrencyPipe, NgForOf} from "@angular/common";
     CurrencyPipe,
     NgForOf
   ],
-  //templateUrl: './product-list.component.html',
   templateUrl: './product-list-grid.component.html',
   styleUrl: './product-list.component.scss'
 })
 export class ProductListComponent implements OnInit{
 
   product: Product[] = [];
-  constructor(private productService: ProductService) {
+  currentCategoryId:number = 1;
+  currentCategoryName: string = "";
+
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) {
   }
 
   ngOnInit(){
-    this.listProducts()
+    this.route.paramMap.subscribe(() => {
+      this.listProducts()
+    })
   }
 
   listProducts(){
-    this.productService.getProductList().subscribe(data => {
+    //check if "id" parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id')
+
+    if(hasCategoryId){
+      //get the id from param and convert to number
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+      // get the "name" param string
+      this.currentCategoryName = this.route.snapshot.paramMap.get('name')!;
+    }else {
+      //not category id available default category id to 1
+      this.currentCategoryId = 1;
+      this.currentCategoryName = 'Books';
+    }
+
+    //get the products with category id
+    this.productService.getProductList(this.currentCategoryId).subscribe(data => {
       this.product = data
     })
   }
